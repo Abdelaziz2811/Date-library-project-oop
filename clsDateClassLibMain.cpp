4294ce92ac0767bffe35b63b53cbed194913ad05
abdelaziz <abdobm2811@gmail.com>
2024-11-21 12:59:21 +0100

    Update clsDateClassLibMain.cpp

--- clsDateClassLibMain.cpp
+++ clsDateClassLibMain.cpp
@@ -1,6 +1,7 @@
+//this is .cpp file where the code of class date is executed.
 #include <iostream>
 #include <string>
-#include "clsDate.h"
+#include "clsDate.h"//include clsDate class.
 
 using namespace std;
 
